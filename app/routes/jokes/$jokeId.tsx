4b18f47d273981ec95fb9d6ab665e993532bf90e
yusangKim <yoo99485@likelion.net>
2022-04-18 16:12:25 +0900

    [Update] SEO with Meta tags

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,7 +1,7 @@
-import type { Joke } from "@prisma/client";
 import type {
   ActionFunction,
   LoaderFunction,
+  MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import {
@@ -10,12 +10,30 @@ import {
   useCatch,
   useParams,
 } from "@remix-run/react";
+import type { Joke } from "@prisma/client";
 
 import { db } from "~/utils/db.server";
 import {
   getUserId,
   requireUserId,
 } from "~/utils/session.server";
+
+export const meta: MetaFunction = ({
+  data,
+}: {
+  data: LoaderData | undefined;
+}) => {
+  if (!data) {
+    return {
+      title: "No joke",
+      description: "No joke found",
+    };
+  }
+  return {
+    title: `"${data.joke.name}" joke`,
+    description: `Enjoy the "${data.joke.name}" joke and much more`,
+  };
+};
 
 type LoaderData = { joke: Joke; isOwner: boolean };
 
@@ -44,7 +62,6 @@ export const action: ActionFunction = async ({
   params,
 }) => {
   const form = await request.formData();
-  // NodeFormData { _fields: { _method: [ 'delete' ] } }
   if (form.get("_method") !== "delete") {
     throw new Response(
       `The _method ${form.get("_method")} is not supported`,
@@ -127,9 +144,7 @@ export function CatchBoundary() {
   }
 }
 
-export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
-
+export function ErrorBoundary() {
   const { jokeId } = useParams();
   return (
     <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
